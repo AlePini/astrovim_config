3bbd570bed6fc82f0cc071137be5970dd060e029
AlePini <ale@alepini.com>
2024-12-03 15:40:49 +0100

    testing mypy

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -26,7 +26,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "black",
-        "mypy",
+        -- "mypy",
         "prettier",
         "jq",
         "stylua",
