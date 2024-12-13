57cefc68704275380affbc5d2554f26ec01dd409
AlePini <ale@alepini.com>
2024-12-13 14:43:11 +0100

    fix: pin astrocommunity

--- lua/community.lua
+++ lua/community.lua
@@ -6,7 +6,11 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
+  {
+    "AstroNvim/astrocommunity",
+    commit = "cf7a7ae0e92bf09f5f7d710113fcad794d3e3b77",
+    pin = true
+  },
   { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python-ruff" },
